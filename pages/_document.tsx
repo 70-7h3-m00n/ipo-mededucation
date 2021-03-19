8001c1f80cb321b982345166daee6d8ab1d67255
Novailoveyou <novailoveoux@gmail.com>
2021-03-19 12:50:52 +0300

    trying to make script tags work on navigation

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,5 +1,28 @@
-  
 import Document, { Html, Head, Main, NextScript } from 'next/document'
+import dynamic from 'next/dynamic'
+
+// if (typeof window !== 'undefined' && typeof window.navigator !== 'undefined') {
+//   import('jquery')
+//   import('slick-carousel')
+//   import('fancybox')
+//   import('jquery-sticky-kit')
+//   // import autocolumnlist
+//   import('masonry-layout')
+//   import('lazy-youtube')
+//   import('../vendors/js/myjs')
+// }
+
+// const Test = dynamic(() => {
+//   return import('../vendors/js/jquery-3.5.1.min.js').then(mod => mod)
+// }, {ssr: false})
+// import('../vendors/js/jquery-3.5.1.min.js')
+// import('../vendors/js/slick.min.js')
+// import('../vendors/js/jquery.fancybox.min.js')
+// import('../vendors/js/stickykit.js')
+// import('../vendors/js/jquery.autocolumnlist.min.js')
+// import('../vendors/js/masonry.min.js')
+// import('../vendors/js/lazyYT.js')
+// import('../vendors/js/myjs.js')
 
 class MyDocument extends Document {
   // static async getInitialProps(ctx) {
